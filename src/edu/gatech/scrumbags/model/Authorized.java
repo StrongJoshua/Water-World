78a3dfdead73ccd4f9eddfa4fbb6fde6ffccfb2a
Jan Risse <jan.risse@hotmail.com>
2016-09-27 22:49:32 -0400

    Added Authorized user creation and password hashing

--- src/edu/gatech/scrumbags/model/Authorized.java
+++ src/edu/gatech/scrumbags/model/Authorized.java
@@ -1,9 +1,20 @@
 package edu.gatech.scrumbags.model;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class Authorized {
 	private String first, last;
 	private String username;
 	private String pass;
 	
+	public Authorized(String first, String last, String username, String pass) {
+		this.first = first;
+		this.last = last;
+		this.username = username;
+		this.pass = BCrypt.hashpw(pass, BCrypt.gensalt());
+	}
 	
+	public boolean authenticate(String username, String pass) {
+		return username.equals(this.username) && BCrypt.checkpw(pass, this.pass);
+	}
 }
