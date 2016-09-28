949528697d6eaf62196be80bf960e3e4410052ea
Jan Risse <jan.risse@hotmail.com>
2016-09-27 22:26:20 -0400

    M4 release; fixed scene paths to work in jar

--- src/edu/gatech/scrumbags/fxapp/MainFXApplication.java
+++ src/edu/gatech/scrumbags/fxapp/MainFXApplication.java
@@ -1,6 +1,7 @@
 package edu.gatech.scrumbags.fxapp;
 
 import java.io.IOException;
+import java.net.URL;
 
 import edu.gatech.scrumbags.model.User;
 import javafx.application.Application;
@@ -11,43 +12,45 @@ import javafx.stage.Stage;
 
 public class MainFXApplication extends Application {
 	public enum Scenes {
-		welcome("../view/WelcomeView.fxml"), login("../view/LoginView.fxml"), loggedIn("../view/LoggedInView.fxml");
-		
+		welcome("/edu/gatech/scrumbags/view/WelcomeView.fxml"), login(
+				"/edu/gatech/scrumbags/view/LoginView.fxml"), loggedIn("/edu/gatech/scrumbags/view/LoggedInView.fxml");
+
 		private String path;
-		
+
 		private Scenes(String path) {
 			this.path = path;
 		}
-		
+
 		private String getPath() {
 			return path;
 		}
 	}
+
 	public static final String version = "0.2.0";
-	
+
 	public static Stage mainStage;
 	public static User userInfo;
-	
-    @Override
-    public void start(Stage primaryStage) {
-        mainStage = primaryStage;
-        loadScene(Scenes.welcome);
-        primaryStage.show();
-    }
-    
-    public static void loadScene(Scenes scene) {
-    	try {
-	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(MainFXApplication.class.getResource(scene.getPath()));
-	        Parent root = loader.load();
-	        mainStage.setTitle("Clean Water by SCRUMbags for CS 2340");
-	        mainStage.setScene(new Scene(root));
-    	} catch(IOException e) {
-    		e.printStackTrace();
-    	}
-    }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		mainStage = primaryStage;
+		loadScene(Scenes.welcome);
+		primaryStage.show();
+	}
+
+	public static void loadScene(Scenes scene) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainFXApplication.class.getResource(scene.getPath()));
+			Parent root = loader.load();
+			mainStage.setTitle("Clean Water by SCRUMbags for CS 2340");
+			mainStage.setScene(new Scene(root));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
