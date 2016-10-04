bde80036b45010fbef32186c868d15fd83889f93
Jan Risse <jan.risse@hotmail.com>
2016-10-03 22:20:10 -0400

    0.5.0.0 M5 completed

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Versions
 ========
 Latest Stable: **0.4.0.0**
 
-Current Development Build **0.4.6.0**
+Current Development Build **0.5.0.0**
 
 ###How the Numbers Work
 ####First Digit
--- src/edu/gatech/scrumbags/fxapp/MainFXApplication.java
+++ src/edu/gatech/scrumbags/fxapp/MainFXApplication.java
@@ -33,7 +33,7 @@ public class MainFXApplication extends Application {
 		}
 	}
 
-	public static final String version = "0.4.6.0";
+	public static final String version = "0.5.0.0";
 
 	public static ArrayList<Authorized> allUsers;
 
