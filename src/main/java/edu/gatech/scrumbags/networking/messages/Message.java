64c207475028ce9730d3147ef953d492a528a505
Jan Risse <jan.risse@hotmail.com>
2016-10-10 15:45:06 -0400

    Messages now require a message type

--- src/main/java/edu/gatech/scrumbags/networking/messages/Message.java
+++ src/main/java/edu/gatech/scrumbags/networking/messages/Message.java
@@ -1,14 +1,21 @@
+
 package edu.gatech.scrumbags.networking.messages;
 
 public class Message {
 	enum MessageType {
-		registration, userInfo
+		registration, login, userInfo
 	}
 
+	private MessageType type;
 	private String[] payload;
 
-	public Message (String... payload) {
+	public Message (MessageType type, String... payload) {
+		this.type = type;
 		this.payload = payload;
+	}
+
+	public MessageType getType () {
+		return type;
 	}
 
 	public String[] getPayload () {
