225c6442cda4ab58cf63dc87569a3fc20401e2ba
Rishi Raj <rajminster@gmail.com>
2016-12-06 15:06:05 -0500

    Added Profile Screen

--- Aquafinda/app/src/main/java/scrumbags/aquafindamobile/HistoricalReportPromptActivity.java
+++ Aquafinda/app/src/main/java/scrumbags/aquafindamobile/HistoricalReportPromptActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 public class HistoricalReportPromptActivity extends AppCompatActivity {
 
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
