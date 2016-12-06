905b1de52ca1e387e6c9b2b483892d031bc94d14
Jan Risse <jan.risse@hotmail.com>
2016-12-06 03:31:35 -0500

    1.1.1.0 made the all reports screen use a table and not just print strings

--- src/main/java/edu/gatech/scrumbags/controller/AllReportsController.java
+++ src/main/java/edu/gatech/scrumbags/controller/AllReportsController.java
@@ -5,34 +5,123 @@ import java.util.List;
 
 import edu.gatech.scrumbags.fxapp.MainFXApplication;
 import edu.gatech.scrumbags.fxapp.MainFXApplication.Scenes;
+import edu.gatech.scrumbags.model.WaterCondition;
+import edu.gatech.scrumbags.model.WaterPurityCondition;
+import edu.gatech.scrumbags.model.WaterPurityReport;
 import edu.gatech.scrumbags.model.WaterReport;
+import edu.gatech.scrumbags.model.WaterSourceReport;
+import edu.gatech.scrumbags.model.WaterType;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 
 /** The controller for the all reports view, the view containing a list of all water reports
  *
  * @author Beau Mitchell */
 public class AllReportsController implements UpdateableController {
-    /*
-     * @FXML private Label descriptionLabel;
-     *
-     * @FXML private Label locationLabel;
-     *
-     * @FXML private Label dateLabel;
-     *
-     * @FXML private Label noReportsLabel;
-     *
-     * @FXML private Label userLabel;
-     *
-     * @FXML private Label conditionLabel;
-     */
-    @FXML private ListView<WaterReport> reportList;
+    @FXML TableView<WaterReport> reportsTable;
+    @FXML TableColumn<WaterReport, Integer> idCol;
+    @FXML TableColumn<WaterReport, String> reportTypeCol;
+    @FXML TableColumn<WaterReport, String> dateCol;
+    @FXML TableColumn<WaterReport, String> submitterCol;
+    @FXML TableColumn<WaterReport, String> locationCol;
+    @FXML TableColumn<WaterReport, WaterType> typeCol;
+    @FXML TableColumn<WaterReport, WaterCondition> conditionCol;
+    @FXML TableColumn<WaterReport, WaterPurityCondition> purityCol;
+    @FXML TableColumn<WaterReport, Double> virusCol;
+    @FXML TableColumn<WaterReport, Double> contaminantCol;
 
     /** Called when the view is initialized. Sets up the list view and binds the labels to the selection model note: reports from
      * MainFXApplication::waterReports are loaded here when the view is loaded. */
     @FXML
     public void initialize () {
-        reportList.getItems().addAll(MainFXApplication.waterReports);
+        idCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, Integer>, ObservableValue<Integer>>() {
+            @Override
+            public ObservableValue<Integer> call (CellDataFeatures<WaterReport, Integer> param) {
+                return new ReadOnlyObjectWrapper<>(param.getValue().getId());
+            }
+        });
+        reportTypeCol
+            .setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call (CellDataFeatures<WaterReport, String> param) {
+                    String s = "";
+                    if (param.getValue() instanceof WaterSourceReport)
+                        s = "Source";
+                    else
+                        s = "Purity";
+                    return new ReadOnlyObjectWrapper<>(s);
+                }
+            });
+        dateCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call (CellDataFeatures<WaterReport, String> param) {
+                return new ReadOnlyObjectWrapper<>(param.getValue().getDateString());
+            }
+        });
+        submitterCol
+            .setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call (CellDataFeatures<WaterReport, String> param) {
+                    return new ReadOnlyObjectWrapper<>(param.getValue().getSubmitterName());
+                }
+            });
+        locationCol
+            .setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call (CellDataFeatures<WaterReport, String> param) {
+                    return new ReadOnlyObjectWrapper<>(param.getValue().getLocationString());
+                }
+            });
+        typeCol.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<WaterReport, WaterType>, ObservableValue<WaterType>>() {
+                @Override
+                public ObservableValue<WaterType> call (CellDataFeatures<WaterReport, WaterType> param) {
+                    if (param.getValue() instanceof WaterSourceReport)
+                        return new ReadOnlyObjectWrapper<>(((WaterSourceReport)param.getValue()).getSourceType());
+                    return null;
+                }
+            });
+        conditionCol.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<WaterReport, WaterCondition>, ObservableValue<WaterCondition>>() {
+                @Override
+                public ObservableValue<WaterCondition> call (CellDataFeatures<WaterReport, WaterCondition> param) {
+                    if (param.getValue() instanceof WaterSourceReport)
+                        return new ReadOnlyObjectWrapper<>(((WaterSourceReport)param.getValue()).getCondition());
+                    return null;
+                }
+            });
+        purityCol.setCellValueFactory(
+            new Callback<TableColumn.CellDataFeatures<WaterReport, WaterPurityCondition>, ObservableValue<WaterPurityCondition>>() {
+                @Override
+                public ObservableValue<WaterPurityCondition> call (CellDataFeatures<WaterReport, WaterPurityCondition> param) {
+                    if (param.getValue() instanceof WaterPurityReport)
+                        return new ReadOnlyObjectWrapper<>(((WaterPurityReport)param.getValue()).getPurityCondition());
+                    return null;
+                }
+            });
+        virusCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, Double>, ObservableValue<Double>>() {
+            @Override
+            public ObservableValue<Double> call (CellDataFeatures<WaterReport, Double> param) {
+                if (param.getValue() instanceof WaterPurityReport)
+                    return new ReadOnlyObjectWrapper<>(((WaterPurityReport)param.getValue()).getVirusPPM());
+                return null;
+            }
+        });
+        contaminantCol
+            .setCellValueFactory(new Callback<TableColumn.CellDataFeatures<WaterReport, Double>, ObservableValue<Double>>() {
+                @Override
+                public ObservableValue<Double> call (CellDataFeatures<WaterReport, Double> param) {
+                    if (param.getValue() instanceof WaterPurityReport)
+                        return new ReadOnlyObjectWrapper<>(((WaterPurityReport)param.getValue()).getContaminantPPM());
+                    return null;
+                }
+            });
+        reportsTable.getItems().addAll(MainFXApplication.waterReports);
     }
 
     /** Returns to the main view when back button is pressed. */
@@ -45,11 +134,11 @@ public class AllReportsController implements UpdateableController {
     @FXML
     public void handleViewPressed () {
         MainFXApplication.loadScene(Scenes.main);
-        MainFXApplication.setMapLocation(reportList.getSelectionModel().getSelectedItem().getLocation());
+        MainFXApplication.setMapLocation(reportsTable.getSelectionModel().getSelectedItem().getLocation());
     }
 
     @Override
     public void updateReports (List<WaterReport> newReports) {
-        reportList.getItems().addAll(newReports);
+        reportsTable.getItems().addAll(newReports);
     }
 }
--- src/main/java/edu/gatech/scrumbags/fxapp/MainFXApplication.java
+++ src/main/java/edu/gatech/scrumbags/fxapp/MainFXApplication.java
@@ -52,7 +52,7 @@ public class MainFXApplication extends Application {
         }
     }
 
-    public static final String version = "1.1.0.2";
+    public static final String version = "1.1.1.0";
 
     public static List<WaterReport> waterReports;
     public static List<Integer> years;
