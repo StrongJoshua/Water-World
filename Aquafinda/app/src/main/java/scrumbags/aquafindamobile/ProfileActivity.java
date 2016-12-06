a44345c509d8ad6c13fb42c7cfbe2f70eabec0ef
Jan Risse <jan.risse@hotmail.com>
2016-12-06 15:51:39 -0500

    Profile fields set correctly

--- Aquafinda/app/src/main/java/scrumbags/aquafindamobile/ProfileActivity.java
+++ Aquafinda/app/src/main/java/scrumbags/aquafindamobile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -16,12 +17,14 @@ public class ProfileActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
+		((TextView)findViewById(R.id.accountType)).setText(Client.user.getAuthorization().toString());
+		((TextView)findViewById(R.id.emailText)).setText(Client.user.getEmail());
+		((TextView)findViewById(R.id.addressText)).setText(Client.user.getAddress());
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+
 			}
 		});
 	}
